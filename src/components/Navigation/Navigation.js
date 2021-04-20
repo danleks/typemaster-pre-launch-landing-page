6ec7b3f24fcb724d4e892a1dda96559a4a61e48f
danleks <danilevich.lks@gmail.com>
2021-04-20 22:44:34 +0200

    Create Navigation

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,26 @@
+import './Navigation.styles.sass';
+import Logo from '../../assets/shared/logo.svg';
+import Button from '../Button/Button';
+
+const buttonComponent = new Reef('[data-component-name="nav-button"]', {template: Button});
+
+const Navigation = new Reef('[data-component-name="navigation"]', {
+    template: () => {       
+        return `
+            <nav>
+                <ul class="navigation">
+                    <li>
+                        <img src=${Logo} alt='Logo icon' />
+                    </li>
+                    <li>
+                        <my-button data-component-name="nav-button" data-button-style="grey">Pre-order now</my-button>
+                    </li>
+                </ul>
+            </nav>
+        `
+    }
+});
+
+Navigation.attach([buttonComponent]);
+
+export default Navigation;
