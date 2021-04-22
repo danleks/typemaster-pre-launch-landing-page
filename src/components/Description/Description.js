7e208289f720717defc1580baf88716c1c894c90
danleks <danilevich.lks@gmail.com>
2021-04-22 21:50:37 +0200

    Update mobile styles

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -6,12 +6,18 @@ const Description = new Reef('[data-component-name="description"]', {
     template: () => {
         return `
             <div class="description">
-                <figure class="imgContainer">
-                    <img src=${phoneAndKeyboard} / alt='Phone and keyboard'>
-                </figure>
-                <figure class="imgContainer">
-                    <img src=${glassAndKeyboard} / alt='Glass and keyboard'>
-                </figure>
+                <div class="description__container">
+                    <figure class="imgContent">
+                        <img src=${phoneAndKeyboard} / alt='Phone and keyboard'>
+                    </figure>
+                    <figure class="imgContent">
+                        <img src=${glassAndKeyboard} / alt='Glass and keyboard'>
+                    </figure>
+                </div>
+                <div class="description__container">
+                    <h2 class="description__title">mechanical wireless keyboard</h2>
+                    <p class="main-text">The Typemaster keyboard boasts top-notch build and practical design. It offers a wide variety of switches and keycaps, along with reliable wireless connectivity.</p>
+                </div>
             </div>
         `;
     }
