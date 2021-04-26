f05b6453ff9d3b76a7dd3bced5d591efda770b54
danleks <danilevich.lks@gmail.com>
2021-04-26 21:28:19 +0200

    Update description desktop styles

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -1,6 +1,11 @@
 import './Description.styles.sass';
-import phoneAndKeyboard from '../../assets/mobile/image-phone-and-keyboard.jpg';
-import glassAndKeyboard from '../../assets/mobile/image-glass-and-keyboard.jpg';
+import mobilePhoneAndKeyboard from '../../assets/mobile/image-phone-and-keyboard.jpg';
+import tabletPhoneAndKeyboard from '../../assets/tablet/image-phone-and-keyboard.jpg';
+import desktopPhoneAndKeyboard from '../../assets/desktop/image-phone-and-keyboard.jpg';
+import mobileGlassAndKeyboard from '../../assets/mobile/image-glass-and-keyboard.jpg';
+import tabletGlassAndKeyboard from '../../assets/tablet/image-glass-and-keyboard.jpg';
+import desktopGlassAndKeyboard from '../../assets/desktop/image-glass-and-keyboard.jpg';
+
 
 const Description = new Reef('[data-component-name="description"]', {
     template: () => {
@@ -8,15 +13,21 @@ const Description = new Reef('[data-component-name="description"]', {
             <div class="description">
                 <div class="description__container">
                     <figure class="imgContent">
-                        <img src=${phoneAndKeyboard} / alt='Phone and keyboard'>
+                        <img src=${mobilePhoneAndKeyboard}
+                                srcset="${mobilePhoneAndKeyboard} 258w, ${tabletPhoneAndKeyboard} 428w, ${desktopPhoneAndKeyboard} 510w"
+                                sizes="(min-width: 1440px) 510px, (min-width: 768px) 428px, (min-width: 320px) 258px"
+                        alt='Phone and keyboard'>
                     </figure>
                     <figure class="imgContent">
-                        <img src=${glassAndKeyboard} / alt='Glass and keyboard'>
+                    <img src=${mobileGlassAndKeyboard}
+                        srcset="${mobileGlassAndKeyboard} 440w, ${tabletGlassAndKeyboard} 888w, ${desktopGlassAndKeyboard} 887w"
+                        sizes="(min-width: 1440px) 887px, (min-width: 768px) 888px, (min-width: 320px) 440px"
+                        alt='Glass and keyboard'>
                     </figure>
                 </div>
                 <div class="description__container">
                     <h2 class="description__title">mechanical wireless keyboard</h2>
-                    <p class="main-text">The Typemaster keyboard boasts top-notch build and practical design. It offers a wide variety of switches and keycaps, along with reliable wireless connectivity.</p>
+                    <p class="description__text main-text">The Typemaster keyboard boasts top-notch build and practical design. It offers a wide variety of switches and keycaps, along with reliable wireless connectivity.</p>
                 </div>
             </div>
         `;
